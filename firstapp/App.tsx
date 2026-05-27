f74b6cca1e06391c234a20d1158fbbf7216b6655
yanseeeeeeeeee <anasemenova56966@gmail.com>
2026-05-27 18:39:17 +0700

    firstapp

---
+++ firstapp/App.tsx
@@ -0,0 +1,33 @@
+import React, {useRef, useState} from 'react'
+import {SafeAreaView, StatusBar, Text} from 'react-native';
+import WebView from 'react-native-webview';
+import { WebViewMessageEvent } from 'react-native-webview';
+
+export default function App() { //главная функция для прорисовки экрана
+  const webViewRef = useRef<WebView>(null); //создали пустую ссылку для дальнейшей работы с ней
+  const [msg, setMsg] = useState("");
+
+  const handleMessage = (event: WebViewMessageEvent) => {
+    const data = JSON.parse(event.nativeEvent.data);
+    console.log("получено: ", data);
+    setMsg(data.message);
+  };
+
+  //ретерн как контейнер для ui компонентов
+  return (
+    //занимаем все пространство экрана
+    <SafeAreaView style = {{flex: 1, padding: 10}}>
+      <StatusBar hidden/>
+
+      <Text>Сообщение: {msg}</Text>
+
+      <WebView
+      //связали с ссылкой
+      ref={webViewRef} 
+      source={{uri: 'http://192.168.0.41:5173/'}}
+      onMessage={handleMessage}
+      />
+
+    </SafeAreaView>
+  );
+}
