02136f3d4afc9cdc5d44a8770a913d3d3beb415e
yanseeeeeeeeee <anasemenova56966@gmail.com>
2026-06-03 19:58:46 +0700

    все что есть

--- firstapp/App.tsx
+++ firstapp/App.tsx
@@ -1,12 +1,37 @@
-import React, {useRef, useState} from 'react'
+import React, {useEffect, useRef, useState} from 'react'
 import {SafeAreaView, StatusBar, Text} from 'react-native';
 import WebView from 'react-native-webview';
 import { WebViewMessageEvent } from 'react-native-webview';
 import { View } from 'react-native';
+import { Alert } from 'react-native';
+
+import messaging from '@react-native-firebase/messaging';
 
 export default function App() { //главная функция для прорисовки экрана
   const webViewRef = useRef<WebView>(null); //создали пустую ссылку для дальнейшей работы с ней
   const [msg, setMsg] = useState("");
+  
+
+  useEffect(() => {
+
+    const setupFCM = async () => {
+      const authStatus = await messaging().requestPermission();
+      console.log('Статус авторизации:', authStatus);
+
+      const fcmToken = await messaging().getToken();
+      console.log('FCM token:', fcmToken);
+
+      messaging().onMessage(async remoteMessage => {
+        Alert.alert(
+          remoteMessage.notification?.title || 'Новое уведомление' ,
+          remoteMessage.notification?.body || ''
+        );
+      });
+    };
+
+    setupFCM();
+  }, []);
+
 
   const handleMessage = (event: WebViewMessageEvent) => {
     const data = JSON.parse(event.nativeEvent.data);
